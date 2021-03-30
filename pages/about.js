81fb62ccf33eb20675f9411e38b58a364729fd21
Esteban Munch Jones <esteban.munch.jones@gmail.com>
2021-03-30 11:01:19 +0100

    move layout import to _app.js

--- pages/about.js
+++ pages/about.js
@@ -2,9 +2,9 @@ import Layout from '../components/layout';
 
 export default function Home() {
   return (
-    <Layout>
+    <>
         <h1>This is the About page</h1>
         <h2>On this demo app, you'll learn the basic difference between React and Next</h2>
-    </Layout>
+    </>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,10 @@ import Layout from '../components/layout';
 
 export default function Home() {
   return (
-    <Layout>
+    <>
       <h1>This is the Home page</h1>
       <h2>This is the Next demo</h2>
       <h2>If you right click on this app, and choose 'View Page Source', you'll see that the HTML content includes this paragraph.</h2>
-    </Layout>
+    </>
   )
 }
